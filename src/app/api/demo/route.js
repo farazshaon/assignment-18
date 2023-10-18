2525cb8885d74d5e563d2078b1ad4042307a0151
farazshaon <farazshaon0@gmail.com>
2023-10-18 15:00:04 +0600

    ok

--- src/app/api/demo/route.js
+++ src/app/api/demo/route.js
@@ -5,22 +5,29 @@ import { PrismaClient } from "@prisma/client";
 
 export async function POST(req, res) {
 
-//! Single Insert Data
+//! Aggregate (avg,count,max,min,sum)
   //   try {
   //     const prisma = new PrismaClient();
-  //     let reqBody = await req.json();
-  //     let result = await prisma.user.create({ data: reqBody });
+  //    const result = await prisma.product.aggregate({
+//       _avg: { salary: true },
+//       _count: { salary: true },
+//       _max: { salary: true },
+//       _min: { salary: true },
+//       _sum: { salary: true },
+// });
 
   //     return NextResponse.json({ status: "Success", result: result });
   //   } catch (err) {
   //     return NextResponse.json({ status: "Fail", result: err.toString() });
   //   }
 
-  //!  Insert Many Data
+  //!  Aggregate (groupBy,having)
   //   try {
   //     const prisma = new PrismaClient();
-  //     let reqBody = await req.json();
-  //     let result = await prisma.post.createMany({ data: reqBody });
+  //     const result = await prisma.order.groupBy({
+//        by: ["city"],
+//        _count: { grandTotal: true },
+// })
 
   //     return NextResponse.json({ status: "Success", result: result });
   //   } catch (err) {
@@ -37,66 +44,25 @@ export async function POST(req, res) {
   //     return NextResponse.json({ status: "Fail", result: err.toString() });
   //   }
 
-  //!  Read Single Data
+
+
+
+  //!  Transactions & Rollback
   //   try {
   //     const prisma = new PrismaClient();
 
-  //     let result = await prisma.tag.findMany({
-  //       skip: 2,
-  //       take: 3,
-  //       select: {
-  //         id: true,
-  //         title: true,
-  //         metaTitle: true,
-  //       },
-  //     });
+  //     const createUser = prisma.user.create({
+  //   data:{email:"johndoe.doe@example.com", password:"123"}
+  // })
+
+  // const deleteComment=prisma.product.delete({
+  //     where:{id:5}
+  // })
   //     return NextResponse.json({ status: "Success", result: result });
   //   } catch (err) {
   //     return NextResponse.json({ status: "Fail", result: err.toString() });
   //   }
 
-  //!  Read Single Data by contains
-  //   try {
-  //     const prisma = new PrismaClient();
-
-  //     let result = await prisma. post_tag.findMany({
-  //       where: {
-  //         tagId: { contains: 1 },
-  //       },
-  //     });
-  //     return NextResponse.json({ status: "Success", result: result });
-  //   } catch (err) {
-  //     return NextResponse.json({ status: "Fail", result: err.toString() });
-  //   }
-
-  //!  Delete Single Data by contains
-  //   try {
-  //     const prisma = new PrismaClient();
-  //     let reqBody = await req.json();
-  //     let result = await prisma.post_meta.delete({
-  //       where: reqBody,
-  //     });
-  //     return NextResponse.json({ status: "Success", result: result });
-  //   } catch (err) {
-  //     return NextResponse.json({ status: "Fail", result: err.toString() });
-  //   }
-
-  //!  Update Single Data by contains
-  //   try {
-  //     const prisma = new PrismaClient();
-  //     let reqBody = await req.json();
-  //     let result = await prisma.category .update({
-  //       where: reqBody,
-  //       data: {
-  //         title: "catagoru title",
-  //         metaTitle: "meta tag",
-  //         
-  //       },
-  //     });
-  //     return NextResponse.json({ status: "Success", result: result });
-  //   } catch (err) {
-  //     return NextResponse.json({ status: "Fail", result: err.toString() });
-  //   }
 
 
 }
